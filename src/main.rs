7c5e63880a5e52fdacecdec779dcf19d67b7be5d
Vaibhav Yenamandra <3663231+envp@users.noreply.github.com>
2022-04-01 22:50:03 -0400

    Discard `main.rs` and update Cargo.toml for publish

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if let Some(file_path) = std::env::args().nth(1) {
-        let data = std::fs::read(file_path).unwrap();
-        dbg!(plist_utils::parse_binary_plist(&data));
-    } else {
-    }
-}
